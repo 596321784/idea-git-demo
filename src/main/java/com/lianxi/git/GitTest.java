8e325013a2d55217b2043f60de008322638c4422
fq <596321784@qq.com>
2022-11-16 11:27:54 +0800

    first commit

---
+++ src/main/java/com/lianxi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lianxi.git;
+
+/**
+ * @author fq
+ * @create 2022-11-16 11:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
